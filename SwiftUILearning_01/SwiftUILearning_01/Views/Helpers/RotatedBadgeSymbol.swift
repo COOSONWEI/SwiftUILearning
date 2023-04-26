744cd265df3ba01314815c81051c558823247091
COSONWEI <2168642113@qq.com>
2023-04-26 15:15:02 +0800

    add Badge

---
+++ SwiftUILearning_01/SwiftUILearning_01/Views/Helpers/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUILearning_01
+//
+//  Created by 韦小新 on 2023/4/26.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 0))
+    }
+}
